fa6d30804e3125202a86c66c42e529ddaea81ea9
tmboroka <113712881+tmboroka@users.noreply.github.com>
2023-10-30 10:20:50 +0100

    set up SpringBoot backend

---
+++ backend/src/main/java/com/example/datafilter/DataFilterApplication.java
@@ -0,0 +1,13 @@
+package com.example.datafilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataFilterApplication.class, args);
+    }
+
+}
