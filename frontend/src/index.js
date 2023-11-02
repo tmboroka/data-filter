f73166dba7b852d5ad241848c5720cd7a60541a2
tmboroka <113712881+tmboroka@users.noreply.github.com>
2023-11-02 14:40:39 +0100

    update index.js files with current routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ErrorPage  from './Pages/ErrorPage';
+import ItemList from './Pages/ItemList';
+import LoginPage from './Pages/LoginPage';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <ErrorPage />,
+    children:[
+      {
+        path:"/",
+        element: <LoginPage />
+      },
+      {
+        path: "/table",
+        element: <ItemList />
+      }
+    
+    ]
+  }
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
